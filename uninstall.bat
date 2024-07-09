1823418217c89275477f22ad563d9794222235c4
Francisco Panis Kaseker <franciscopk@ni-sp.com>
2024-07-09 11:44:28 -0300

    also stop python app

--- uninstall.bat
+++ uninstall.bat
@@ -4,11 +4,12 @@ REM Execute PowerShell uninstallation scripts
 REM Set execution policy to bypass for the current process
 powershell.exe -Command "Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass"
 
-REM Execute remove_service.ps1
-echo Executing remove_service.ps1...
+REM Execute remove_services.ps1
+echo Executing remove_services.ps1...
+powershell.exe -File .\stop_dcvm_app.ps1
 powershell.exe -File .\remove_services.ps1
 if %errorlevel% neq 0 (
-    echo Error executing remove_service.ps1
+    echo Error executing remove_services.ps1
     exit /b %errorlevel%
 )
 
